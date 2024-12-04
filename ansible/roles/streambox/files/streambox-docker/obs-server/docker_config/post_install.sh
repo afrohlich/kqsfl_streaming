2b9ee384efad66dc3d0e3fcb5d4a442e97bfab21
Arthur <4741387+afrohlich@users.noreply.github.com>
2024-12-04 14:53:28 -0500

    wip

--- ansible/roles/streambox/files/streambox-docker/obs-server/docker_config/post_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-apt-get update
-# Install remote desktop (nomachine,kasmvnc,novnc)
-bash /docker_config/install_kasmvnc.sh
---
+++ streambox-docker/obs-server/scripts/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec /dockerstartup/kasm_default_profile.sh /dockerstartup/vnc_startup.sh /dockerstartup/kasm_startup.sh $@
